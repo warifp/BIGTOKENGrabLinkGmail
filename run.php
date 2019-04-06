2edc0425bb8905220141325f0d66eb5a8bc475e8
Wahyu Arif Purnomo <warifp@gmail.com>
2019-04-07 00:30:35 +0700

    Update run.php

--- run.php
+++ run.php
@@ -1,15 +1,15 @@
 <?php
 
-    $email = array(
-            'host' 		=> '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX',
-            'username' 	=> 'masukkan email',
-            'password' 	=> 'masukkan password'
-        );
+    $host = "{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX";
+    echo "Email \t\t: ";
+    $email = trim(fgets(STDIN)) or die("!email harus di isi dengan benar.");
+    echo "Password \t: ";
+    $password = trim(fgets(STDIN)) or die("!password harus di isi dengan benar.");
 
-     $read = imap_open($email['host'],$email['username'],$email['password']);
-     $array = imap_search($read,'SUBJECT "BIGtoken"');
+    $read = imap_open($host,$email,$password);
+    $array = imap_search($read,'SUBJECT "BIGtoken"');
      
-     foreach($array as $a){
+    foreach($array as $a){
         $header = imap_header($read, $a);
         $html = imap_qprint(imap_fetchbody($read, $a, 1));
         
@@ -19,3 +19,4 @@
         $link = $links[1]->getAttribute('href')."\n";  
         file_put_contents('link.txt', $link, FILE_APPEND | LOCK_EX);
      }
+?>
